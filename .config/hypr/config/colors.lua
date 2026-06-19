be8e5d1f131fd242daa1794332f839942897dc86
iamagut <57557058+iamagut@users.noreply.github.com>
2026-06-19 10:56:02 +0700

    Create colors.lua

---
+++ .config/hypr/config/colors.lua
@@ -0,0 +1,9 @@
+local colors = {
+    background = "rgb(121317)",
+    primary    = "rgb(aec6ff)",
+    accent     = "rgb(f7bb7c)",
+    inactive   = "rgba(44474fcc)",
+    active     = "rgba(aec6ffff)",
+}
+
+return colors
